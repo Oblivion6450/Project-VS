1c3844c5c1b248eed3a5a899b65ebbc1c74728d9
Oblivion6450 <96287613+Oblivion6450@users.noreply.github.com>
2022-08-06 19:40:56 +0500

    008

--- vahin-shop 2/js/main.js
+++ vahin-shop 2/js/main.js
@@ -1,12 +1,12 @@
 const images = document.querySelectorAll('.slider .slider_line img');
-const sliderLine = document.querySelector('slider_line');
+const sliderLine = document.querySelector('.slider_line');
 let index = 0;
 let width;
 
-function init(){
+function init() {
     console.log('resize');
     width = document.querySelector('.slider').offsetWidth;
-    sliderLine.style.width = width * images.length +'px';
+    sliderLine.style.width = width * images.length + 'px';
     images.forEach(item => {
         item.style.width = width + 'px';
         item.style.height = 'auto';
@@ -17,22 +17,40 @@ function init(){
 window.addEventListener('resize', init);
 init();
 
-document.querySelector('slider_prev').addEventListener('click', function(){
-    index--;
-    if (index < 0){
-        index = images.length - 1;
-    }
-    rollSlider();
-});
+// document.querySelector('radio').addEventListener('click', function () {
+//     index--;
+//     if (index < 0) {
+//         index = images.length - 1;
+//     }
+//     rollSlider();
+// });
 
-document.querySelector('slider_next').addEventListener('click', function(){
+// document.querySelector('.slider_next').addEventListener('click', function () {
+//     index++;
+//     if (index >= images.length) {
+//         index = 0;
+//     }
+//     rollSlider();
+// });
+
+function rollSlider() {
+    sliderLine.style.transform = 'translate(-' + index * width + 'px)';
+}
+
+setInterval(function () {
+    rollSlider(index)
     index++;
-    if (index >= images.length){
+    if (index == 4) {
         index = 0;
     }
-    rollSlider();
-});
+}, 5000)
 
-function rollSlider(){
-    sliderLine.style.transform = 'translate(-'+index*width+ 'px)';
-}
+// function slides_number(index) {
+//     var dots = document.querySelectorAll(".dot")
+
+//     for (var index = 0; index < dots.length; index++) {
+//         dots[index].className = dots[index].className.replace("cheked", "");
+
+//         dots[index - 1].className += " cheked";
+//     }
+// }
